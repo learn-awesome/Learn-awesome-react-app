d58a3e10d0fef56651a0d61d4c1b0f16541077da
eshnil <team@polyglot.network>
2019-11-24 14:11:33 +0530

    fetch topics from graphql

---
+++ pages/TopicList.js
@@ -0,0 +1,11 @@
+const TopicList = ({ topics }) => (
+    <div>
+      {topics && topics.map((a, i) => (
+        <div key={i}>
+          <h2>{a.name}</h2>
+        </div>
+      ))}
+    </div>
+  )
+  
+  export default TopicList;
