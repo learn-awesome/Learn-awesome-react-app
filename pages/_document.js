bf05957b444c9df3194070bdc5d0b0dc53c244b5
lastnamearya <arya.jigyasu6815@gmail.com>
2019-12-14 16:00:34 +0530

    Styled-components ~ SSR config (_document.js_

---
+++ pages/_document.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Document, { Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+export default class MyCustomDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+
+    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
+
+    const styleTags = sheet.getStyleElement();
+
+    return { ...page, styleTags }; // return styles collected
+  }
+
+  render() {
+    return (
+      <html lang="en-us">
+        <Head>{this.props.styleTags}</Head>
+        <body style={{ margin: '0px' }}>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,17 @@
-import { Query } from 'react-apollo';
-import withData from '../config';
+import Head from 'next/head';
 
-import TopicList from './TopicList';
-import GET_TOPICS from '../graphql/Topics';
-import NetworkLoader from '../utils/NetworkLoader';
+import withData from '../config';
 
 // Global Styles
 import GlobalStyles from '../styles/GlobalStyles';
 
-const Index = () => {
-  return (
-    <Query // <- Wrapping the main component with Query component from react-apollo
-      query={GET_TOPICS}
-      fetchPolicy={'cache-and-network'}
-    >
-      {({ loading, data, error }) => {
-        if (loading) return <NetworkLoader />;
-        if (error) return <p>Somethign went wrong.</p>;
-
-        if (data && data.topics && data.topics.length >= 1) {
-          return (
-            <div>
-              <GlobalStyles />
-              <h1>Topics</h1>
-              <TopicList topics={data.topics} />
-            </div>
-          );
-        }
-      }}
-    </Query>
-  );
-};
+const Index = () => (
+  <div>
+    <Head>
+      <title>Learnawesome</title>
+    </Head>
+    <GlobalStyles />
+  </div>
+);
 
 export default withData(Index);
